fbfd233a4ed63986392d77224066114871f12fa1
Elvis Serrão <elvis.serrao1@gmail.com>
2020-04-15 01:29:31 -0300

    Create Dashboard controller and jbuilder

---
+++ test/controllers/api/v1/dashboard_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+module Api
+  module V1
+    class DashboardControllerTest < ActionDispatch::IntegrationTest
+      # test "the truth" do
+      #   assert true
+      # end
+    end
+  end
+end
